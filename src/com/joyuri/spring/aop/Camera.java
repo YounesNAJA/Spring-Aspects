987e993d98a5b66e5fefd3d7bd0c38b351c2ca87
Younes NAJA <naja.younes@gmail.com>
2016-10-21 02:08:38 +0100

    Completing annotations and deleting XML

--- src/com/joyuri/spring/aop/Camera.java
+++ src/com/joyuri/spring/aop/Camera.java
@@ -2,8 +2,10 @@ package com.joyuri.spring.aop;
 
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class Camera {
+	
 	public void snap(){
 		System.out.println("SNAP!");
 	}
--- src/com/joyuri/spring/aop/Logger.java
+++ src/com/joyuri/spring/aop/Logger.java
@@ -1,15 +1,20 @@
 package com.joyuri.spring.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 /*
  * This class is called Aspect
  */
+@Aspect
 @Component
 public class Logger {
+	
 	/*
 	 * This method is called Advice
 	 */
+	@Before("execution(void com.joyuri.spring.aop.Camera.snap())")
 	public void PreSnap(){
 		System.out.println("Someone is about to take a photo.");
 	}
