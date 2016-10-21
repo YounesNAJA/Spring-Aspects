419170b6314e57ffeaab115b143e231b5a00a7b5
Younes NAJA <naja.younes@gmail.com>
2016-10-21 02:16:36 +0100

    Reusable pointcut for CameraSnap

--- src/com/joyuri/spring/aop/Logger.java
+++ src/com/joyuri/spring/aop/Logger.java
@@ -2,6 +2,7 @@ package com.joyuri.spring.aop;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 /*
@@ -10,12 +11,19 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class Logger {
-	
+
+	/*
+	 * Reusable pointcut
+	 */
+	@Pointcut("execution(void com.joyuri.spring.aop.Camera.snap())")
+	public void cameraSnap() {
+	}
+
 	/*
 	 * This method is called Advice
 	 */
-	@Before("execution(void com.joyuri.spring.aop.Camera.snap())")
-	public void PreSnap(){
+	@Before("cameraSnap()")
+	public void PreSnap() {
 		System.out.println("Someone is about to take a photo.");
 	}
 }
